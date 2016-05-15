6644e67050dc5a0837ffea39363031d1978994a7
Jonas Windey <jonas.windey@gmail.com>
2016-05-15 16:52:06 +0200

    Update tests

--- test/actions/files.js
+++ test/actions/files.js
@@ -10,7 +10,7 @@ const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 const {API} = constants
 
-const store = mockStore({
+const initialStore = {
   files: {
     files: [],
     filesLoaded: false,
@@ -19,7 +19,11 @@ const store = mockStore({
     project: constants.DEFAULT_PROJECT,
     queue: 0,
   }
-})
+}
+let store = {}
+
+const filesURI = (project = initialStore.files.project) =>
+  `/files?pagesize=0&filters=project:${project}%2Bstatus:active`
 
 describe('file actions', () => {
 
@@ -27,16 +31,66 @@ describe('file actions', () => {
     nock.cleanAll()
   })
 
+  beforeEach(() => {
+    store = mockStore(initialStore)
+  })
+
   it('should fetch files', async () => {
-    nock(API).get('/files?pagesize=0&filters=project:jonas%2Bstatus:active').reply(200, { items: [{_id: -1}] })
+    nock(API).get(filesURI()).reply(200, { items: [{_id: -1}] })
 
     await store.dispatch(fileActions.fetchFiles())
-
-    const expectedActions = [
-      { type: constants.FETCH_FILES_START },
-      { type: constants.FETCH_FILES_COMPLETE,  files: [{_id: -1}] }
-    ]
-
-    expect(store.getActions()).toEqual(expectedActions)
+    expect(store.getActions()).toEqual([
+      {type: constants.FETCH_FILES_START},
+      {type: constants.FETCH_FILES_COMPLETE, files: [{_id: -1}]}
+    ])
   })
+
+  it('should fetch files and throw error when url is invalid', async () => {
+    nock(API).get(filesURI()).reply(500)
+    await store.dispatch(fileActions.fetchFiles())
+    expect(store.getActions()).toEqual([
+      {type: constants.FETCH_FILES_START},
+      {type: constants.FETCH_FILES_FAILED},
+    ])
+  })
+
+  it('should change project', async () => {
+    nock(API).get(filesURI('test')).reply(200, { items: [{_id: -1}] })
+    await store.dispatch(fileActions.changeProject('test'))
+    expect(store.getActions()).toEqual([
+      {type: constants.CHANGE_PROJECT},
+      {type: constants.FETCH_FILES_START},
+    ])
+  })
+
+  it('should throw error when changing project and url is invalid', async () => {
+    nock(API).get(filesURI('test')).reply(500)
+    await store.dispatch(fileActions.changeProject('test'))
+    expect(store.getActions()).toEqual([
+      {type: constants.CHANGE_PROJECT},
+      {type: constants.FETCH_FILES_START},
+    ])
+  })
+
+  it('should remove file and fetch files', async () => {
+
+    nock(API).get(filesURI()).reply(200, { items: [{_id: -1}] })
+    nock(API).delete(`/in/${initialStore.files.project}/-1`).reply(200, { items: [{_id: -1}] })
+    await store.dispatch(fileActions.remove(-1))
+    expect(store.getActions()).toEqual([
+      {type: constants.REMOVE_FILE_START},
+      {type: constants.REMOVE_FILE_COMPLETE},
+      {type: constants.FETCH_FILES_START},
+    ])
+  })
+
+  it('should throw error when removing file and url is invalid', async () => {
+    nock(API).delete('/in/test/-1').reply(500)
+    await store.dispatch(fileActions.remove(-1))
+    expect(store.getActions()).toEqual([
+      {type: constants.REMOVE_FILE_START},
+      {type: constants.REMOVE_FILE_FAILED},
+    ])
+  })
+
 })
--- test/test.js
+++ test/test.js
@@ -1,2 +1,6 @@
 import debug from 'debug'
+
 debug.enable('dev')
+debug.enable('test')
+
+debug('test')('Starting tests')
